bcdc91d872a1879cb28fa447e0177b6fb81571a2
Damir Nabiullin <51910382+DamirNabiull@users.noreply.github.com>
2023-02-08 16:04:56 +0300

    Update Program.cs

--- InterpreterOfLisp/Program.cs
+++ InterpreterOfLisp/Program.cs
@@ -1,8 +1,15 @@
-﻿namespace InterpreterOfLisp;
+﻿using InterpreterOfLisp.Lexer;
+using InterpreterOfLisp.FileProcessing;
+
+namespace InterpreterOfLisp;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var fr = new FileReader(@"C:\Users\Damir\Documents\Compilers Construction\AdditionalFiles\test.lsp");
+        var text = fr.GetText();
+        
+        var tokenizer = new Tokenizer(text);
+        tokenizer.GetAllTokens();
     }
 }
