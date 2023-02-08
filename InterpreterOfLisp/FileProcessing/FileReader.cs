026e37ae82134b380eebfb40a4eae13930172642
Damir Nabiullin <51910382+DamirNabiull@users.noreply.github.com>
2023-02-08 16:04:38 +0300

    Created FileReader

---
+++ InterpreterOfLisp/FileProcessing/FileReader.cs
@@ -0,0 +1,16 @@
+﻿namespace InterpreterOfLisp.FileProcessing;
+
+public class FileReader
+{
+    private readonly string _text;
+
+    public FileReader(string path)
+    {
+        _text = File.ReadAllText(path).ReplaceLineEndings(" ");
+    }
+
+    public string GetText()
+    {
+        return _text;
+    }
+}
