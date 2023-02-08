93def98d08badfcfdd898128f2a7ac9e1d1dc247
Damir Nabiullin <51910382+DamirNabiull@users.noreply.github.com>
2023-02-08 15:39:49 +0300

    Small fixes

--- InterpreterOfLisp/Lexer/Tokenizer.cs
+++ InterpreterOfLisp/Lexer/Tokenizer.cs
@@ -1,17 +1,24 @@
-﻿namespace InterpreterOfLisp.Lexer;
+﻿using System.Text.RegularExpressions;
+
+namespace InterpreterOfLisp.Lexer;
 
 public class Tokenizer
 {
     private readonly string _text;
     private int _position = 0;
-    private char _currentChar;
     
     public Tokenizer(string text)
     {
         _text = text;
     }
 
-    public Token GetToken()
+    public void GetAllTokens()
+    {
+        while (_position < _text.Length)
+            GetToken();
+    }
+
+    private Token GetToken()
     {
         object value = GetNextSyntaxElement();
         var start = _position;
@@ -28,18 +35,39 @@ public class Tokenizer
             case "\0":
                 code = TokenCode.EofTk;
                 break;
+            case "quote":
+                code = TokenCode.QuoteTk;
+                break;
+            case "\'":
+                code = TokenCode.QuoteTk;
+                break;
+            default:
+                code = TokenCode.IdentifierTk;
+                break;
         }
         
         Console.WriteLine($"{value} - {code}");
 
         return new Token(start, _position, code, value);
     }
+    
+    // Можно уточнить, нужен ли нам span, если нет, то используем это
+    public void GetAllSyntaxElements()
+    {
+        var matchList = Regex.Matches(_text, @"[()']|[a-zA-Z0-9]+|[+-][1]");
+        var list = matchList.Cast<Match>().Select(match => match.Value).ToList();
+        foreach (var el in list)
+        {
+            Console.WriteLine(el);
+        }
+    }
 
     private string GetNextSyntaxElement()
     {
-        ReadNextChar();
-        var value = _currentChar.ToString();
-
+        SkipSpaces();
+        var value = GetCurrentChar().ToString();
+        _position++;
+    
         switch (value)
         {
             case "(":
@@ -49,23 +77,28 @@ public class Tokenizer
             case "\0":
                 break;
             default:
-                while (_currentChar != '\0' && _currentChar != '(' && _currentChar != ')')
+                while (GetCurrentChar() != '\0' 
+                       && GetCurrentChar() != '(' 
+                       && GetCurrentChar() != ')' 
+                       && GetCurrentChar() != ' ')
                 {
-                    ReadNextChar();
-                    value += _currentChar;
+                    value += GetCurrentChar();
+                    _position++;
                 }
                 break;
         }
-
+    
         return value;
     }
 
-    private void ReadNextChar()
+    private void SkipSpaces()
     {
         while (_text[_position] == ' ')
             _position++;
-        
-        _currentChar = _text[_position];
-        _position++;
+    }
+
+    private char GetCurrentChar()
+    {
+        return _text[_position];
     }
 }
